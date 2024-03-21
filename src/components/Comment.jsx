43b07412d493d4dedb54a3280b59114c90ab47b3
Juan Del Pozo <83425295+juan-delpozo@users.noreply.github.com>
2024-03-21 15:42:50 -0400

    Passed props to Comment component

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function Comment(){
+function Comment(props){
     return(
         <div>
-            <p>Test Comment</p>
+            <p>{props.content}</p>
         </div>
     )
 }
